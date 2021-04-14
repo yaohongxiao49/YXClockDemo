bce9c0d52185215f5403816cf1490825f7c4cbf6
yaohongxiao <617146817@qq.com>
2021-04-14 14:19:38 +0800

    change：游走/跳动两种模式兼容

--- YXClockTest/ViewController.m
+++ YXClockTest/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) YXClockView *clockView;
+@property (nonatomic, strong) YXClockModel *model;
+
 @end
 
 @implementation ViewController
@@ -22,32 +25,43 @@
     
     UIImage *img = [UIImage imageNamed:@"YXClockImg"];
     
-    YXClockModel *model = [[YXClockModel alloc] init];
-    model.type = YXClockModelGoTypeContinuous;
-    model.clockBgImg = img;
+    _model = [[YXClockModel alloc] init];
+    _model.type = YXClockModelGoTypeContinuous;
+    _model.clockBgImg = img;
     
-    model.hoursDegreeColor = [UIColor whiteColor];
-    model.hoursDegreeCorner = 2;
-    model.hoursDegreeWidth = 2;
-    model.hoursDegreeMargin = 58;
+    _model.hoursDegreeColor = [UIColor whiteColor];
+    _model.hoursDegreeCorner = 2;
+    _model.hoursDegreeWidth = 2;
+    _model.hoursDegreeMargin = 58;
     
-    model.minutesDegreeColor = [UIColor whiteColor];
-    model.minutesDegreeCorner = 1;
-    model.minutesDegreeWidth = 1.5;
-    model.minutesDegreeMargin = 38;
+    _model.minutesDegreeColor = [UIColor whiteColor];
+    _model.minutesDegreeCorner = 1;
+    _model.minutesDegreeWidth = 1.5;
+    _model.minutesDegreeMargin = 38;
     
-    model.secondsDegreeColor = [UIColor whiteColor];
-    model.secondsDegreeCorner = 0;
-    model.secondsDegreeWidth = 1;
-    model.secondsDegreeMargin = 10;
+    _model.secondsDegreeColor = [UIColor whiteColor];
+    _model.secondsDegreeCorner = 0;
+    _model.secondsDegreeWidth = 1;
+    _model.secondsDegreeMargin = 10;
     
-    model.centerDegreeColor = [UIColor whiteColor];
-    model.centerDegreeWidth = 10;
-    model.centerDegreeCorner = 5;
+    _model.centerDegreeColor = [UIColor whiteColor];
+    _model.centerDegreeWidth = 10;
+    _model.centerDegreeCorner = 5;
     
-    YXClockView *view = [[YXClockView alloc] initWithFrame:CGRectMake(0, 0, 175, 175) model:model];
-    view.center = self.view.center;
-    [self.view addSubview:view];
+    _clockView = [[YXClockView alloc] initWithFrame:CGRectMake(0, 0, 175, 175) model:_model];
+    _clockView.center = self.view.center;
+    [self.view addSubview:_clockView];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    if (_model.type == YXClockModelGoTypeDiscontinuous) {
+        _model.type = YXClockModelGoTypeContinuous;
+    }
+    else {
+        _model.type = YXClockModelGoTypeDiscontinuous;
+    }
+    _clockView.model = _model;
 }
 
 @end
